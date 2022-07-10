71fd9f5104c7c30db77a6dff98400531302902c5
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2022-07-10 18:28:53 +0300

    Refactoring all svg icons

--- js/preloader.js
+++ js/preloader.js
@@ -2,8 +2,7 @@
 window.onload = function () {
     let preloader = document.getElementById('preloader');
     document.body.classList.remove('_lock');
-    preloader.style.display = 'none';
-    // preloader.style.opacity = '0';
-    // preloader.style.visibility = 'hidden';
+    preloader.style.opacity = '0';
+    preloader.style.visibility = 'hidden';
 
 }

8b38b14bf9316c9238f18ba56681401b623fdcbe
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2022-07-10 17:23:14 +0300

    Refactoring
    - svg added directly to html code;

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,9 @@
 'use strict';
 window.onload = function () {
     let preloader = document.getElementById('preloader');
-    preloader.style.opacity = '0';
-    preloader.style.visibility = 'hidden';
     document.body.classList.remove('_lock');
+    preloader.style.display = 'none';
+    // preloader.style.opacity = '0';
+    // preloader.style.visibility = 'hidden';
+
 }
